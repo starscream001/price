9b3eb1c70e0070987ad074af60632d6cc1d3c812
Ilya <manik1990@list.ru>
2025-11-24 16:08:06 +0300

    update photo

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const products = [
     tag: 'Блеск и защита',
     price: '30 мл',
     note: 'Для финальной шлифовки и домашнего ухода.',
-    image: 'https://images.unsplash.com/photo-1524592094714-0f0654e20314?auto=format&fit=crop&w=800&q=60',
+    image: 'https://optim.tildacdn.com/stor6239-6234-4066-b264-316336383561/-/format/webp/77603545.jpg.webp',
     link: 'https://pollylook.com/catalog/tproduct/818258500-915383082642-maslo-molekulyarnoe-smart-dlya-polirovki'
   },
   {
